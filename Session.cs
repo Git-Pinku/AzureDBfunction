5e348b306780f759b82913c7e3296995b673695a
Git-Pinku <59430873+Git-Pinku@users.noreply.github.com>
2022-09-25 17:10:39 +0530

    data processing

---
+++ Session.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+
+namespace processcsvdata
+{
+    public class Session : TableEntity
+    {
+        public Session()
+        {
+        }
+
+        public Session(string subject, string instructor)
+        {
+            PartitionKey = subject;
+            RowKey = instructor;
+        }
+
+        public string Lectures { get; set; }
+        public string Labs { get; set; }
+        public double Points { get; set; }
+        public bool IsWeekend { get; set; }
+    }
+}
